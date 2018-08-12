aab408becdcfa467f54ea678a446c79eb7e8a11b
s <s@arch.(none)>
2018-08-13 01:23:45 +0700

    reformat

--- pkglist/gen.sh
+++ pkglist/gen.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo -ne "# Official Packages:\n"
-yaourt -Qe | egrep -v '^(core|base|local)' | tee arch
+yaourt -Qe | egrep -v '^(core|base|local)' | awk '{print $1}'| tee arch
 
 echo -ne "\n# AUR Packages:\n"
-yaourt -Qe | egrep ^local | tee aur
+yaourt -Qe | egrep ^local | awk '{print $1}' | tee aur
 
 echo -ne "\nDone!!\n"

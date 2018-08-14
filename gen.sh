dace687e118c7f97d9fa65797245b372161212c2
s <s@archbang>
2018-08-14 17:25:13 +0700

    gen.sh

--- gen.sh
+++ gen.sh
@@ -3,7 +3,7 @@
 mkdir -p pkglist
 
 echo -ne "# Official Packages:\n"
-yaourt -Qe | egrep -v '^(core|base|local)' | awk '{print $1}'| tee pkglist/arch
+yaourt -Qe | egrep '^extra|^community' | awk '{print $1}'| tee pkglist/arch
 
 echo -ne "\n# AUR Packages:\n"
 yaourt -Qe | egrep ^local | awk '{print $1}' | tee pkglist/aur
